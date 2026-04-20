40a8d6e4267f3d6f41708043ab4666ba84bb6ba2
harshith27kk <harshithh348@gmail.com>
2026-04-20 23:41:11 +0530

    Linked list example

---
+++ Learning/LinkedListExample.java
@@ -0,0 +1,20 @@
+package Learning;
+import java.util.LinkedList;
+
+public class LinkedListExample {
+
+    public static void main(String[] args){
+        
+        // Creating a LinkedList
+        LinkedList<String> l = new LinkedList<String>();
+
+        // Adding elements to the LinkedList using add() method
+        l.add("One");
+        l.add("Two");
+        l.add("Three");
+        l.add("Four");
+        l.add("Five");
+
+        System.out.println(l);
+    }
+}
